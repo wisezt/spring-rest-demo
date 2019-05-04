396c41078e99d4fee14345d038d5898fa221b9bf
Ting <wisezt@hotmail.com>
2019-05-04 13:40:44 -0500

    477. All good

--- src/main/java/com/luv2code/springdemo/rest/StudentRestController.java
+++ src/main/java/com/luv2code/springdemo/rest/StudentRestController.java
@@ -1,10 +1,9 @@
 package com.luv2code.springdemo.rest;
 
 import com.luv2code.springdemo.entity.Student;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.PostConstruct;
 import java.util.ArrayList;
@@ -43,8 +42,27 @@ public class StudentRestController {
     @GetMapping("/students/{studentId}")
     public Student getStudent(@PathVariable int studentId){
 
+        // check the sutdentId against list size
+
+        if ((studentId >= theStudents.size()) || (studentId <0)){
+            throw new StudentNotFoundException("Stduent id not found - " + studentId);
+        }
+
+
+
         return theStudents.get(studentId);
 
+    }
+
+    // Add an excepitonhandler using @ExceptionHandler
+    @ExceptionHandler
+    public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException exc){
+        StudentErrorResponse theStudentErrorResponse = new StudentErrorResponse();
+        theStudentErrorResponse.setMessage(exc.getMessage());
+        theStudentErrorResponse.setStatus(HttpStatus.NOT_FOUND.value());
+        theStudentErrorResponse.setTimeStamp(System.currentTimeMillis());
+
+        return new ResponseEntity<>(theStudentErrorResponse, HttpStatus.NOT_FOUND);
     }
 
 

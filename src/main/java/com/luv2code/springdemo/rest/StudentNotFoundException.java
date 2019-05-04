6089cd0540c7fc2bf1a2c97866c38d5f7e33ea97
Ting <wisezt@hotmail.com>
2019-05-04 13:31:08 -0500

    474. Add Constructors for StudentNotFoundException

--- src/main/java/com/luv2code/springdemo/rest/StudentNotFoundException.java
+++ src/main/java/com/luv2code/springdemo/rest/StudentNotFoundException.java
@@ -5,4 +5,19 @@ public class StudentNotFoundException extends RuntimeException {
     public StudentNotFoundException(String message) {
         super(message);
     }
+
+    public StudentNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public StudentNotFoundException(Throwable cause) {
+        super(cause);
+    }
+
+    public StudentNotFoundException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+
+    public StudentNotFoundException() {
+    }
 }

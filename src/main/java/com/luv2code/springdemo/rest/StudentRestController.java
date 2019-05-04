f74e4a382761bcc5734fa29f6a3642387f67e17f
Ting <wisezt@hotmail.com>
2019-05-03 20:42:01 -0500

    467. Add theStudents List

--- src/main/java/com/luv2code/springdemo/rest/StudentRestController.java
+++ src/main/java/com/luv2code/springdemo/rest/StudentRestController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -14,6 +15,11 @@ public class StudentRestController {
     // define endpoint for "/students" - return list of students
     @GetMapping("/students")
     public List<Student> getStudents(){
-        return null;
+
+        List<Student> theStudents = new ArrayList<>();
+        theStudents.add(new Student("Ting001", "Zheng001"));
+        theStudents.add(new Student("Ting002", "Zheng002"));
+        theStudents.add(new Student("Ting003", "Zheng003"));
+        return theStudents;
     }
 }

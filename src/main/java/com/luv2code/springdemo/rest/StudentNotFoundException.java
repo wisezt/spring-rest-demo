229f1611bd922734c181d0774cf4cb6b59c4f822
Ting <wisezt@hotmail.com>
2019-05-04 13:30:06 -0500

    474. Add StudentNotFoundException

---
+++ src/main/java/com/luv2code/springdemo/rest/StudentNotFoundException.java
@@ -0,0 +1,8 @@
+package com.luv2code.springdemo.rest;
+
+public class StudentNotFoundException extends RuntimeException {
+
+    public StudentNotFoundException(String message) {
+        super(message);
+    }
+}
